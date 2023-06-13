ce48318745ec550c8a6429cda89aa98c38d83b36
csb <csb@csb.com>
2023-06-13 11:30:31 +0900

    과제

--- src/variable/Ex_test.java
+++ src/variable/Ex_test.java
@@ -8,10 +8,10 @@ public static void main(String[] args) {
 	int iq = 120;
 	char grade = 'B';
 	
-	System.out.println("이름     : " + name);
-	System.out.println("나이     : " + age);
-	System.out.println("키      : " + height);
-	System.out.println("아이큐    : " + iq);
-	System.out.println("등급     : " + grade);
+	System.out.println("이름\t: " + name);
+	System.out.println("나이\t: " + age);
+	System.out.println("키\t: " + height);
+	System.out.println("아이큐\t: " + iq);
+	System.out.println("등급\t: " + grade);
 }
 }
