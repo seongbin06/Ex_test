7328f652196cf8c9bd68905ea6498696d23b27f6
csb <csb@csb.com>
2023-06-13 11:24:36 +0900

    과제

---
+++ src/variable/Ex_test.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Ex_test {
+public static void main(String[] args) {
+	String name = "김말이";
+	int age = 20;
+	double height = 173.3;
+	int iq = 120;
+	char grade = 'B';
+	
+	System.out.println("이름     : " + name);
+	System.out.println("나이     : " + age);
+	System.out.println("키      : " + height);
+	System.out.println("아이큐    : " + iq);
+	System.out.println("등급     : " + grade);
+}
+}
